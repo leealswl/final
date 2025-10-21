46b0b285f248e94fa1d2a3844c75fd0716b69439
leealswl <alswl54321600@gmail.com>
2025-10-21 17:12:44 +0900

    asdf

--- frontend/src/pages/home/Homepage.jsx
+++ frontend/src/pages/home/Homepage.jsx
@@ -1,24 +1,12 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import Navbar from './components/Navbar';
 
 const Homepage = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+};
 
-  const editpage =()=>{
-    window.location.href='/edit'
-  }
-  return (
-<<<<<<< HEAD
-    <div>Homepage - 배치만 
-      <button onClick={editpage}>에딧페이지 </button>
-      {/* npm i @mui/material @emotion/react @emotion/styled @mui/icons-material @mui/x-tree-view */}
-=======
-    <div>
-        <Navbar />
-
-
->>>>>>> b0c6a8c (navbar 수정)
-    </div>
-  )
-}
-
-export default Homepage
+export default Homepage;
