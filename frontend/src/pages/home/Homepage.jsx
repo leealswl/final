2c5c21b25cee5921b01a707dc44eeb62ed29d4a8
leealswl <alswl54321600@gmail.com>
2025-10-21 17:12:44 +0900

    asdf

--- frontend/src/pages/home/Homepage.jsx
+++ frontend/src/pages/home/Homepage.jsx
@@ -3,22 +3,11 @@ import Navbar from './components/Navbar'
 
 const Homepage = () => {
 
-  const editpage =()=>{
-    window.location.href='/edit'
-  }
   return (
-<<<<<<< HEAD
-    <div>Homepage - 배치만 
-      <button onClick={editpage}>에딧페이지 </button>
-      {/* npm i @mui/material @emotion/react @emotion/styled @mui/icons-material @mui/x-tree-view */}
-=======
     <div>
         <Navbar />
-
-
->>>>>>> dev
     </div>
   )
-}
+  }
 
 export default Homepage
