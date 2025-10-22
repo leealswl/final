56256fa40cb800a41471b4c20244e6d43fd72943
wizdmoon <wizd.mooncre@gmail.com>
2025-10-22 16:14:40 +0900

    document controller 작업중

--- backend/src/main/java/com/example/backend/domain/User.java
+++ backend/src/main/java/com/example/backend/domain/User.java
@@ -1,5 +1,7 @@
 package com.example.backend.domain;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
@@ -7,6 +9,11 @@ public class User {
     private Long idx;
     private String userId;
     private String userPw;
+    private String userName;
+    private Long roleLevel;
+    private Date createdAt;
+    private Date updatedAt;
+    
 
     public void pwEncoding(String userPw){
         this.userPw = userPw;
---
+++ backend/src/main/java/com/example/backend/mapper/DocumentMapper.java
@@ -0,0 +1,13 @@
+package com.example.backend.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.backend.domain.Document;
+
+
+
+@Mapper
+public interface DocumentMapper {
+    public int insertDocument(Document document);
+    
+}
