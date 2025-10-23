8687d8a20ba2bfea732f54054c93e5c559e25dad
wizdmoon <wizd.mooncre@gmail.com>
2025-10-23 16:54:01 +0900

    controller 수정

--- backend/src/main/java/com/example/backend/mapper/ProjectMapper.java
+++ backend/src/main/java/com/example/backend/mapper/ProjectMapper.java
@@ -1,5 +1,7 @@
 package com.example.backend.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -9,9 +11,10 @@ import com.example.backend.domain.Project;
 public interface ProjectMapper {
 
     public Project getProject(@Param("projectIdx") Long projectidx);
+    public List<Project> getProjectList(@Param("userIdx") Long useridx);
 
     public int insertProject(Project project);
-    public int updateProject(Project project);
+    public int updateProjectName(Project project);
     public int deleteProject(Project project);
     
 }
