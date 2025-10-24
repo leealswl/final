e1ded43f891960aa5fc5c5437496683ef6854f95
wizdmoon <wizd.mooncre@gmail.com>
2025-10-24 17:55:43 +0900

    controller 수정

--- backend/src/main/java/com/example/backend/service/DocumentServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/DocumentServiceImpl.java
@@ -29,12 +29,14 @@ public class DocumentServiceImpl implements DocumentService{
     @Override
     public int saveFilesAndDocuments(List<MultipartFile> files, List<Long> folders, String userid, Long projectIdx) throws IOException {
         System.out.println("document service 작동 시작");
+        System.out.println("uploadDir: " + uploadDir);
         int totalInserted = 0;
         for (int i = 0; i < files.size(); i++) {
             MultipartFile file = files.get(i);
             Long folderName = folders.get(i);
             
             Path uploadPath = Paths.get(uploadDir, userid, String.valueOf(projectIdx), String.valueOf(folderName));
+            System.out.println("uploadPath: " + uploadPath);
             if (!Files.exists(uploadPath)) {
                 Files.createDirectories(uploadPath);
             } //절대경로로 저장
