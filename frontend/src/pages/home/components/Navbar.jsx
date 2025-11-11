3c74d9094481792fe0205d5032ac2e2e898a65f0
wizdmoon <wizd.mooncre@gmail.com>
2025-11-11 11:32:08 +0900

    login, logout

--- frontend/src/pages/home/components/Navbar.jsx
+++ frontend/src/pages/home/components/Navbar.jsx
@@ -1,79 +1,131 @@
-import React from 'react'
-import {Button,  Grid, Stack, Box, Typography} from "@mui/material";
-import logo from '../img/nav_logo.png'
+import React from 'react';
+import { Button, Grid, Stack, Box, Typography, CircularProgress } from '@mui/material';
+import logo from '../img/nav_logo.png';
+import { useAuthStore } from '../../../store/useAuthStore';
+import useSignOut from '../../../hooks/userSignOut';
+import { useNavigate } from 'react-router';
 
 const Navbar = () => {
+    const user = useAuthStore((s) => s.user);
+    const redirectTo = location.state?.from || '/'; // 이전 페이지 or 홈
+    const navigate = useNavigate();
 
-  const edit =() => {
-    window.location.href="/edit"
-  }
+    const edit = () => {
+        window.location.href = '/works';
+    };
 
-  const login = () => {
-    window.location.href="/login"
-  }
+    const login = () => {
+        window.location.href = '/login';
+    };
 
-  return (
-    <Grid container pt={3} mx={30}>
+    const { mutate: signOut, isPending } = useSignOut({
+        onSuccess: () => navigate(redirectTo, { replace: true }),
+    });
 
-      <Grid size={1} display="flex" justifyContent="center" alignItems="center">
-        <Box
-          component="img"
-          src={logo}
-          alt="logo"
-          sx={{
-            mr: '10px',
-            width: 30,
-            height: "auto",
-            // borderRadius: 2,
-            // boxShadow: 3,
-          }}
-    />
-    <Typography fontSize={24} fontWeight={'bold'}>Paladoc</Typography>
-      </Grid>
-      <Grid size={9} display="flex" justifyContent="center" alignItems="center">
-        <Stack direction="row" spacing={3}>
-          <Button size='large' color='warning' sx={{color: 'black', '&:hover': {backgroundColor: '#f5f5f5'}}}>카탈로그</Button>
-          <Button size='large' color='success' sx={{color: 'black', '&:hover': {backgroundColor: '#f5f5f5'}}}>요금제</Button>
-          <Button size='large' color='primary' sx={{color: 'black', '&:hover': {backgroundColor: '#f5f5f5'}}}>AI</Button>
-          <Button size='large' color='error' sx={{color: 'black', '&:hover': {backgroundColor: '#f5f5f5'}}}>고객지원</Button>
-          <Button size='large' color='secondary' sx={{color: 'black', '&:hover': {backgroundColor: '#f5f5f5'}}} onClick={edit}><strong>시작하기</strong></Button>
-        </Stack>
-      </Grid>
-      <Grid size={2} display="flex" justifyContent="center" alignItems="center">
-        <Stack direction="row" spacing={2}>
-          <Button
-            variant="outlined"
-            onClick={login}
-            sx={{
-              color: 'black',
-              borderColor: 'black',
-              // fontWeight: 'bold',
-              '&:hover': {
-                borderColor: 'black',
-                backgroundColor: '#f5f5f5',
-              },
-            }}
-          >
-            LOG IN
-          </Button>
+    return (
+        <Grid container pt={3} mx={30}>
+            <Grid size={1} display="flex" justifyContent="center" alignItems="center">
+                <Box
+                    component="img"
+                    src={logo}
+                    alt="logo"
+                    sx={{
+                        mr: '10px',
+                        width: 30,
+                        height: 'auto',
+                        // borderRadius: 2,
+                        // boxShadow: 3,
+                    }}
+                />
+                <Typography fontSize={24} fontWeight={'bold'}>
+                    Paladoc
+                </Typography>
+            </Grid>
+            <Grid size={9} display="flex" justifyContent="center" alignItems="center">
+                <Stack direction="row" spacing={3}>
+                    <Button size="large" color="warning" sx={{ color: 'black', '&:hover': { backgroundColor: '#f5f5f5' } }}>
+                        카탈로그
+                    </Button>
+                    <Button size="large" color="success" sx={{ color: 'black', '&:hover': { backgroundColor: '#f5f5f5' } }}>
+                        요금제
+                    </Button>
+                    <Button size="large" color="primary" sx={{ color: 'black', '&:hover': { backgroundColor: '#f5f5f5' } }}>
+                        AI
+                    </Button>
+                    <Button size="large" color="error" sx={{ color: 'black', '&:hover': { backgroundColor: '#f5f5f5' } }}>
+                        고객지원
+                    </Button>
+                    <Button size="large" color="secondary" sx={{ color: 'black', '&:hover': { backgroundColor: '#f5f5f5' } }} onClick={edit}>
+                        <strong>시작하기</strong>
+                    </Button>
+                </Stack>
+            </Grid>
+            <Grid size={2} display="flex" justifyContent="center" alignItems="center">
+                <Stack direction="row" spacing={2}>
+                    {user ? (
+                        <>
+                            <Typography>{user.userName}님</Typography>
+                            <Button
+                                variant="outlined"
+                                onClick={() => signOut()}
+                                disabled={isPending}
+                                sx={{
+                                    color: 'black',
+                                    borderColor: 'black',
+                                    // fontWeight: 'bold',
+                                    '&:hover': {
+                                        borderColor: 'black',
+                                        backgroundColor: '#f5f5f5',
+                                    },
+                                }}
+                            >
+                                {isPending ? (
+                                    <>
+                                        <CircularProgress size={18} sx={{ mr: 1 }} />
+                                        로그아웃 중...
+                                    </>
+                                ) : (
+                                    'LOG OUT'
+                                )}
+                            </Button>
+                        </>
+                    ) : (
+                        <>
+                            <Button
+                                variant="outlined"
+                                onClick={login}
+                                sx={{
+                                    color: 'black',
+                                    borderColor: 'black',
+                                    // fontWeight: 'bold',
+                                    '&:hover': {
+                                        borderColor: 'black',
+                                        backgroundColor: '#f5f5f5',
+                                    },
+                                }}
+                            >
+                                LOG IN
+                            </Button>
 
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: 'black',
-              color: 'white',
-              // fontWeight: 'bold',
-              '&:hover': {
-                backgroundColor: '#333',
-              },
-            }}
-          >
-            SIGN UP
-          </Button>
-        </Stack>
-      </Grid>
-    </Grid>
-  )
-}
+                            <Button
+                                variant="contained"
+                                sx={{
+                                    backgroundColor: 'black',
+                                    color: 'white',
+                                    // fontWeight: 'bold',
+                                    '&:hover': {
+                                        backgroundColor: '#333',
+                                    },
+                                }}
+                            >
+                                SIGN UP
+                            </Button>
+                        </>
+                    )}
+                </Stack>
+            </Grid>
+        </Grid>
+    );
+};
 
-export default Navbar
+export default Navbar;
